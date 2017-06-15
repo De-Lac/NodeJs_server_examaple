c1aae7148b88c76577078a404094d236d036be5c
daniele fanì <daniele.fani@unicam.it>
2017-06-15 10:33:55 +0200

    file di config con i parametri di configurazione del server

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = 
+{
+    'secret':   'questa_e_la_mia_chiave',
+    'database': 'mongodb://localhost:27017/jwt_tutorial_db',
+    'default-admin-name':   'admin',
+    'default-admin-psw':    "admin"
+};
+
